33ac9e270ee23c61ac14d8431ff212f001986de4
Nurel <nurel32@gmail.com>
2020-03-03 20:16:59 +0600

    initial commit

---
+++ precache-manifest.462213740dc0fe5837bce7965c51765b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24737a6684e97d9ae8a840644971d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cafcc8f411992f721341",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "2e0042960d32423de006",
+    "url": "/static/css/main.052741f1.chunk.css"
+  },
+  {
+    "revision": "cafcc8f411992f721341",
+    "url": "/static/js/2.36367d19.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.36367d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0042960d32423de006",
+    "url": "/static/js/main.f005f298.chunk.js"
+  },
+  {
+    "revision": "b4a9d50d628c7b33283a",
+    "url": "/static/js/runtime-main.11d58c34.js"
+  },
+  {
+    "revision": "c7bbc01f9c8c77e91c92ad62b91bbb70",
+    "url": "/static/media/growth-tool-example.c7bbc01f.png"
+  }
+]);
